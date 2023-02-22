5210eee1af8908de5bceed2120676913d37985e6
elektron1k <fop.holoborshchyi@gmail.com>
2023-02-22 15:15:18 +0200

    project environment prepared

---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
+import { rootSaga } from './sagas';
+
+const sagaMiddleware = createSagaMiddleware();
+
+export default configureStore({
+  reducer: {},
+  middleware: [sagaMiddleware],
+});
+
+sagaMiddleware.run(rootSaga);
